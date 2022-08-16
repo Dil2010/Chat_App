d8307ed92438fe06ed004428f693a3e1e80afed7
Dilan Udawattha <iamdilanudawattha@gmail.com>
2022-08-15 22:34:15 -0700

    Updates

--- server/index.js
+++ server/index.js
@@ -1,56 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const authRoutes = require("./routes/auth");
-// const messageRoutes = require("./routes/messages");
-// const app = express();
-// const socket = require("socket.io");
-// require("dotenv").config();
-
-// app.use(cors());
-// app.use(express.json());
-
-// mongoose
-//   .connect(process.env.MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("DB Connetion Successfull");
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//   });
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
-
-// const server = app.listen(process.env.PORT, () =>
-//   console.log(`Server started on ${process.env.PORT}`)
-// );
-// const io = socket(server, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     credentials: true,
-//   },
-// });
-
-// global.onlineUsers = new Map();
-// io.on("connection", (socket) => {
-//   global.chatSocket = socket;
-//   socket.on("add-user", (userId) => {
-//     onlineUsers.set(userId, socket.id);
-//   });
-
-//   socket.on("send-msg", (data) => {
-//     const sendUserSocket = onlineUsers.get(data.to);
-//     if (sendUserSocket) {
-//       socket.to(sendUserSocket).emit("msg-recieve", data.msg);
-//     }
-//   });
-// });
-
-
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -63,12 +10,15 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(() => {
+  })
+  .then(() => {
     console.log("DB Connetion Successfull");
-  }).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err.message);
   });
 
@@ -80,7 +30,8 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
   },
 });
@@ -95,7 +46,7 @@ io.on("connection", (socket) => {
   socket.on("send-msg", (data) => {
     const sendUserSocket = onlineUsers.get(data.to);
     if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
     }
   });
 });
